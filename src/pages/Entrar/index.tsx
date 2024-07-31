227a4bbf985a15452315ebd80b17b6d44a02b7da
victorpss <victorpss@dcc.ufrj.br>
2024-07-31 19:05:03 -0300

    created login page

--- src/pages/Entrar/index.tsx
+++ src/pages/Entrar/index.tsx
@@ -1,9 +1,75 @@
-import {  } from "./style";
+import { useState } from "react";
+import { Container, Content, InputField, Input, InputImage, LoginHeader, Logo, SetaIcon, Title, EsqueciSenha, Text, CadastreSe, EntrarComSocial, SocialButton } from "./style";
 
-export default function Entrar(){
+import { Button } from "../NaoLogado/style";
+import { CustomLink } from "../../components/HeaderHome/style";
+
+import setaIcon from "../../assets/icons/setaVoltar.png";
+import logo from "../../assets/elektroLogo.png";
+import emailIcon from "../../assets/icons/email.png";
+import cadeadoIcon from "../../assets/icons/cadeado.png";
+import olhoBarradoIcon from "../../assets/icons/olhoBarrado.png";
+import olhoNaoBarradoIcon from "../../assets/icons/olhoNaoBarrado.png";
+import facebookIcon from "../../assets/icons/facebookLogo.png";
+import googleIcon from "../../assets/icons/googleLogo.png";
+
+export default function Entrar() {
+    const [passwordVisible, setPasswordVisible] = useState(false);
+
+    const togglePasswordVisibility = () => {
+        setPasswordVisible(!passwordVisible);
+    };
 
     return (
-        <></>         
+        <Container>
+            <LoginHeader>
+                <CustomLink to="/naologado">
+                    <SetaIcon src={setaIcon} alt="Seta para voltar" />
+                </CustomLink>
+                <Title>Entrar</Title>
+            </LoginHeader>
+            <Content>
+                <Logo src={logo} alt="Logotipo do aplicativo" />
+                <InputField>
+                    <InputImage src={emailIcon} alt="Ícone de e-mail" />
+                    <Input type="text" placeholder="Digite seu e-mail" />
+                </InputField>
 
+                <InputField>
+                    <InputImage src={cadeadoIcon} alt="Ícone de cadeado" />
+                    <Input
+                        type={passwordVisible ? "text" : "password"}
+                        placeholder="*********"
+                    />
+                    <InputImage
+                        src={passwordVisible ? olhoNaoBarradoIcon : olhoBarradoIcon}
+                        alt="Ícone de olho"
+                        onClick={togglePasswordVisibility}
+                        style={{ cursor: 'pointer', right: '10px' }}
+                    />
+                </InputField>
+                <EsqueciSenha>Esqueci minha senha</EsqueciSenha>
+                
+                <CustomLink to="/">
+                    <Button>Entrar</Button>
+                </CustomLink>
+
+                <EntrarComSocial>
+                    <CustomLink to="/">
+                        <SocialButton>
+                            <InputImage src={facebookIcon} alt="Ícone do Facebook"></InputImage>
+                        </SocialButton>
+                    </CustomLink>
+                    <Text>ou</Text>
+                    <CustomLink to="/">
+                        <SocialButton>
+                            <InputImage src={googleIcon} alt="Ícone do Google"></InputImage>
+                        </SocialButton>
+                    </CustomLink>
+
+                </EntrarComSocial>
+                <CadastreSe>Não possui cadastro? Cadastre-se!</CadastreSe>
+            </Content>
+        </Container>
     );
 }
--- src/pages/Entrar/style.tsx
+++ src/pages/Entrar/style.tsx
@@ -1,5 +1,116 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-    
+export const Container = styled.main`
+    width: 100%;
+    height: 100vh;
+    background-color: #F5F5F5;
+`;
+
+export const LoginHeader = styled.div`
+    width: 100%;
+    height: 60px;
+    border-bottom: 1.5px solid #B9B9B9;
+    display: flex;
+    justify-content: start;
+    align-items: center;
+    position: relative;
+`;
+
+export const SetaIcon = styled.img`
+    width: 24px;
+    height: 24px;
+    margin-left: 32px;
+`;
+
+export const Title = styled.h1`
+    font-family: "Montserrat", sans-serif;
+    font-weight: 500;
+    font-size: 24px;
+    text-align: center;
+    color: #2B2B2B;
+    margin: 0;
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    text-align: center;
+`;
+
+export const Content = styled.section`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const Logo = styled.img`
+    width: 329.95px;
+    height: 329.95px;
+`;
+
+export const InputField = styled.div`
+    position: relative;
+    display: flex;
+    justify-content: start;
+    align-items: center;
+    margin-bottom: 20px;
+`;
+
+export const Input = styled.input`
+    width: 326px;
+    height: 40px;
+    font-family: "Montserrat", sans-serif;
+    font-weight: 400;
+    font-size: 16px;
+    background-color: transparent;
+    border: 0;
+    border-bottom: 1.5px solid #B9B9B9;
+    text-indent: 28px;
+    padding-right: 40px;
+    box-sizing: border-box;
+`;
+
+export const InputImage = styled.img`
+    width: 24px;
+    height: 24px;
+    position: absolute;
+    cursor: pointer;
+    user-select: none;
+`;
+
+export const Text = styled.h5`
+    font-family: "Montserrat", sans-serif;
+    font-weight: 400;
+    font-size: 14px;
+    color: #2b2b2b;
+`;
+
+export const EsqueciSenha = styled(Text)`
+    color: black;
+    text-decoration: underline;
+    margin: 0 0px 20px 45%;
+    cursor: pointer;
 `
+
+export const EntrarComSocial = styled.div`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    width: 90%;
+    margin: 20px 0;
+`;
+
+export const SocialButton = styled.div`
+    width: 68.99px;
+    height: 68.99px;
+    background-color: #2B2B2B; 
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const CadastreSe = styled(Text)`
+    color: #0271A0;
+    text-decoration: underline;
+    cursor: pointer;
+`;
