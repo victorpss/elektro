6b43f689081cd26c033633401acaf8906ef9fe39
victorpss <victorpss@dcc.ufrj.br>
2024-07-31 19:03:55 -0300

    created footer component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,40 @@
+import { Container, Option, OptionImage, OptionText } from "./style";
+import { Link } from "react-router-dom";
+
+import homeIcon from "../../assets/icons/home.png"
+import chatIcon from "../../assets/icons/chatBranco.png"
+import avisosIcon from "../../assets/icons/avisos.png"
+import perfilIcon from "../../assets/icons/perfil.png"
+
 export default function Footer(){
     return (
-        <div>oi</div>
+        <Container>
+            <Option>
+                <Link to="/">
+                    <OptionImage src={homeIcon}></OptionImage>
+                </Link>
+                <OptionText>Home</OptionText>
+            </Option>
+            <Option>
+                <Link to="/naologado">
+                    <OptionImage src={chatIcon}></OptionImage>
+                </Link>
+                <OptionText>Chat</OptionText>
+            </Option>
+            <Option>
+                <Link to="/naologado">
+                    <OptionImage src={avisosIcon}></OptionImage>
+                </Link>
+                <OptionText>Avisos</OptionText>
+            </Option>
+            <Option>
+                <Link to="/naologado">
+                    <OptionImage src={perfilIcon}></OptionImage>
+                </Link>
+                <OptionText>Perfil</OptionText>
+            </Option>
+
+        </Container>
 
     );
 }
--- src/components/Footer/style.tsx
+++ src/components/Footer/style.tsx
@@ -1,1 +1,33 @@
 import styled from "styled-components";
+
+
+export const Container = styled.footer`
+    width: 100%;
+    height: 68px;
+    background-color: #2B2B2B;
+    position: fixed;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    bottom: 0;
+`;
+
+export const Option = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+`;
+
+export const OptionImage = styled.img`
+    width: 24px;
+    height: 24px;
+`;
+
+export const OptionText = styled.h5`
+    font-family: "Montserrat", sans-serif;
+    font-weight: 400;
+    font-size: 14px;
+    color: #DEDEDE;
+    margin: 0;
+`;
