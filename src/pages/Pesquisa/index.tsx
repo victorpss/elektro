79ff89527534ba9f3a7be365b25ffe97b2af9db9
victorpss <victorpss@dcc.ufrj.br>
2024-08-01 02:34:30 -0300

    created pesquisa page

--- src/pages/Pesquisa/index.tsx
+++ src/pages/Pesquisa/index.tsx
@@ -1,13 +1,66 @@
 import Footer from "../../components/Footer";
 import HeaderGeral from "../../components/HeaderGeral";
-import { Container } from "./style";
+import { Filtro, PesquisaContainer, PesquisaIcon, PesquisaInput, PesquisaInputField } from "../TV/style";
+import { Button, Container, Content, Historico, Pesquisado, Sugestao, SugestaoButton, SugestaoImage, Sugestoes, Text } from "./style";
+import { Icon } from "../../components/HeaderGeral/style";
+import { CustomLink } from "../../components/HeaderHome/style";
+
+import lupaIcon from "../../assets/icons/lupaCinza.png";
+import gFiltroIcon from "../../assets/icons/gFiltro.png";
+import Divisor from "../../components/Divisor";
+import apagarIcon from "../../assets/icons/apagar.png";
+
+import celularesImg from "../../assets/sugestoes/sugestaoCelulares.png";
+import tecladosImg from "../../assets/sugestoes/sugestaoTeclados.png";
+import monitoresImg from "../../assets/sugestoes/sugestaoMonitores.png";
 
 
 export default function Pesquisa(){
     return (
         <Container>
             <HeaderGeral text="Pesquisa"/>
-            
+            <Content>
+                <PesquisaContainer>
+                    <PesquisaInputField>
+                        <PesquisaIcon src={lupaIcon} />
+                        <PesquisaInput type="text" placeholder="Encontrar na Elektro"/>
+                    </PesquisaInputField>
+                    <Filtro src={gFiltroIcon}></Filtro>
+                </PesquisaContainer>
+                    <Historico>
+                        <Pesquisado>
+                            <Text>Monitor</Text>
+                            <Icon src={apagarIcon}></Icon>
+                        </Pesquisado>
+                        <Pesquisado>
+                            <CustomLink to="/tv">
+                                <Text>Smart TV</Text>
+                            </CustomLink>
+                            <Icon src={apagarIcon}></Icon>
+                        </Pesquisado>
+                        <Pesquisado>
+                            <Text>Mousepad</Text>
+                            <Icon src={apagarIcon}></Icon>
+                        </Pesquisado>
+                    </Historico>
+                    <Divisor width={90}></Divisor>
+                    <Text selecionado>Apagar Histórico de Pesquisa</Text>
+                    <Button>Sugestão de busca</Button>
+                    <Sugestoes>
+                        <Sugestao>
+                            <SugestaoImage src={celularesImg}></SugestaoImage>
+                            <SugestaoButton>Celulares</SugestaoButton>
+                        </Sugestao>
+                        <Sugestao>
+                            <SugestaoImage src={tecladosImg}></SugestaoImage>
+                            <SugestaoButton>Teclados</SugestaoButton>
+                        </Sugestao>
+                        <Sugestao>
+                            <SugestaoImage src={monitoresImg}></SugestaoImage>
+                            <SugestaoButton>Monitores</SugestaoButton>
+                        </Sugestao>
+                    </Sugestoes>
+            </Content>
             <Footer/>
         </Container>      
 
--- src/pages/Pesquisa/style.tsx
+++ src/pages/Pesquisa/style.tsx
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
+interface textProps{
+    selecionado?: boolean;
+}
+
+export const Container = styled.main`
     width: 100%;
     height: 100vh;
     display: flex;
@@ -10,3 +14,90 @@ export const Container = styled.div`
     background-color: #F5F5F5;
     overflow-x: hidden;
 `
+
+export const Content = styled.section`
+    width: 90%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+    align-items: center;
+    padding-top: 100px;
+`;
+
+export const Historico = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const Pesquisado = styled.div`
+    width: 80%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 12px 0;
+`;
+
+export const Text = styled.h2<textProps>`
+    font-family: "Montserrat", sans-serif;
+    font-weight: 400;
+    font-size: 14px;
+    color: ${(props) => (props.selecionado? "#0271A0" : "#2B2B2B")};
+    text-decoration: ${(props) => (props.selecionado? "underline" : "none")};
+    margin: ${(props) => (props.selecionado? 20 : 0)};
+`;
+
+export const Button = styled.button`
+    width: 190px;
+    height: 37px;
+    border: 0;
+    border-radius: 16px;
+    background-color: #2B2B2B;
+    color: #F5F5F5;
+    font-family: "Montserrat", sans-serif;
+    font-weight: 400;
+    font-size: 14px;
+    display: flex;
+    justify-content: start;
+    align-items: center;
+    padding-left: 10px;
+    margin: 15px 100px 15px 0;
+`;
+
+export const Sugestoes = styled.div`
+`;
+
+export const Sugestao = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 8px 0px;
+    gap: 20px;
+`;
+
+export const SugestaoImage = styled.img`
+    width: 80px;
+    height: 72px;
+`;
+
+export const SugestaoButton = styled.button`
+    width: 188px;
+    height: 40px;
+    border-radius: 16px;
+    color: #2B2B2B;
+    font-family: "Montserrat", sans-serif;
+    font-weight: 500;
+    font-size: 14px;
+    background-color: #F5F5F5;
+    border: 0;
+    box-shadow: 0px 4px 4px 0px rgba(0,0,0,0.08);
+    transition: background-color 0.3s ease;
+    cursor: pointer;
+
+    &:hover {
+        background-color: #DEDEDE;
+    }
+`;
