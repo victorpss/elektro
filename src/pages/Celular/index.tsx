1c4aa8d06869f722bc2b9e9fbc1077d8756ab74f
victorpss <victorpss@dcc.ufrj.br>
2024-07-31 23:39:05 -0300

    created celular page

--- src/pages/Celular/index.tsx
+++ src/pages/Celular/index.tsx
@@ -1,9 +1,54 @@
-import {  } from "./style";
+import HeaderGeral from "../../components/HeaderGeral";
+import { Avaliacao, CaracteristicasContainer, CaracteristicasItens, CaracteristicasTitulo, ClienteImage, ClienteInfo, ClienteNome, ComprarButton, Container, Content, Icon, Icons, Info, Item, ProdutoContainer, ProdutoImage, ProdutoInfo, ProdutoPreco, ProdutoTitulo } from "./style";
+
+import celularImage from "../../assets/produtos/celularFull.png"
+import favoritoIcon from "../../assets/icons/favoritoBranco.png";
+import carrinhoIcon from "../../assets/icons/carrinhoBranco.png";
+import clienteImage from "../../assets/cliente.png";
+import avaliacao from "../../assets/estrelinhas.png"
+import Footer from "../../components/Footer";
+import { CustomLink } from "../../components/HeaderHome/style";
 
 export default function Celular(){
-
     return (
-        <></>         
+        <Container>
+            <HeaderGeral text="Produto"/>
+            <Content>
+                <ProdutoContainer>
+                    <Icons>
+                        <ProdutoImage src={celularImage}></ProdutoImage>
+                        <Icon src={favoritoIcon}></Icon>
+                        <Icon src={carrinhoIcon}></Icon>
+                    </Icons>
+                    <Info>
+                        <ProdutoInfo>
+                            <ProdutoTitulo>Celular</ProdutoTitulo>
+                            <ProdutoPreco>R$ 800,00</ProdutoPreco>
+                        </ProdutoInfo>
+                        <ClienteInfo>
+                            <ClienteImage src={clienteImage}></ClienteImage>
+                            <ClienteNome>João Ribeiro</ClienteNome>
+                            <Avaliacao src={avaliacao}></Avaliacao>
+                        </ClienteInfo>
+                    </Info>
+                </ProdutoContainer>
+                <CaracteristicasContainer>
+                    <CaracteristicasTitulo>
+                        Características do Produto
+                    </CaracteristicasTitulo>
+                    <CaracteristicasItens>
+                        <Item>Tela Super Retina de 6,1";</Item>
+                        <Item>Chip biônico A15 p/ desempenho + rápido;</Item>
+                        <Item>Sistema avançado de câmera dupla;</Item>
+                        <Item>Câmera frontal TrueDepth de 12MP, com modo noturno e gravação 4K Dolby Vision.</Item>
+                    </CaracteristicasItens>
+                </CaracteristicasContainer>
+                <CustomLink to="/entrar">
+                    <ComprarButton>Comprar</ComprarButton>
+                </CustomLink>
+            </Content>
+            <Footer/>
+        </Container>      
 
     );
 }
--- src/pages/Celular/style.tsx
+++ src/pages/Celular/style.tsx
@@ -1,5 +1,158 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-    
+export const Container = styled.main`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: #F5F5F5;
+    overflow-x: hidden;
+`;
+
+export const Content = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+export const ProdutoContainer = styled.section`
+    width: 100%;
+    height: calc(326+71)px;
+    padding-top: 71px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #2B2B2B;
+    border-radius: 0 0 20px 20px;
+    flex-direction: column;
+`;
+
+
+export const ProdutoImage = styled.img`
+    width: 228px;
+    height: 206px;
+`;
+
+export const Icons = styled.div`
+    width: auto;
+    height: auto;
+    display: flex;
+    align-items: start;
+    gap: 4px;
+    padding: 12px 0 21px 6%;
+
+`;
+
+export const Icon = styled.img`
+    width: 24px;
+    height: 24px;
+    cursor: pointer;
 `
+
+export const Info = styled.div`
+    display: flex;
+    width: 90%;
+    justify-content: space-around;
+    margin-bottom: 15px;
+`;
+
+export const ProdutoInfo = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+    align-items: start;
+    gap: 12px;
+`
+
+export const ProdutoTitulo = styled.h1`
+    font-family: "Montserrat", sans-serif;
+    font-weight: 500;
+    font-size: 24px;
+    color: #F5F5F5;
+    margin: 0;
+`;
+
+export const ProdutoPreco = styled.h1`
+    font-family: "Montserrat", sans-serif;
+    font-weight: 500;
+    font-size: 24px;
+    color: #F5F5F5;
+    margin: 0;
+`;
+
+export const ClienteInfo = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+    align-items: end;
+    gap: 6px;
+`;
+
+export const ClienteImage = styled.img`
+    width: 56px;
+    height: 56px;
+`;
+
+export const ClienteNome = styled.h2`
+    font-family: "Montserrat", sans-serif;
+    font-weight: 500;
+    font-size: 14px;
+    color: #F5F5F5;
+    margin: 0;
+`;
+
+export const Avaliacao = styled.img`
+    width: 137.26px;
+    height: 24px;
+`;
+
+export const CaracteristicasContainer = styled.div`
+    width: 82%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-top: 36px;
+    margin-bottom: 12px;
+`;
+
+export const CaracteristicasTitulo = styled.h1`
+    font-family: "Montserrat", sans-serif;
+    font-weight: 500;
+    font-size: 24px;
+    color: #2B2B2B;
+    margin: 0;
+    text-align: center;
+`;
+
+
+export const CaracteristicasItens = styled.ul`
+    font-family: "Montserrat", sans-serif;
+    font-weight: 400;
+    font-size: 14px;
+    padding: 0;
+`;
+
+export const Item = styled.li`
+    &::marker {
+        font-size: 0.5em; 
+        color: #2B2B2B; 
+    }
+`;
+
+export const ComprarButton = styled.button`
+    width: 188px;
+    height: 40px;
+    border-radius: 16px;
+    border: 0;
+    background-color: #2B2B2B;
+    font-family: "Montserrat", sans-serif;
+    font-weight: 400;
+    font-size: 14px;
+    color: #F5F5F5;
+    cursor: pointer;
+`;
