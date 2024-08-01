ed8bf6e6cb89678644730feb900a3a68fda63ea2
victorpss <victorpss@dcc.ufrj.br>
2024-07-31 23:39:28 -0300

    initializated page

--- src/pages/Pesquisa/index.tsx
+++ src/pages/Pesquisa/index.tsx
@@ -1,9 +1,15 @@
-import {  } from "./style";
+import Footer from "../../components/Footer";
+import HeaderGeral from "../../components/HeaderGeral";
+import { Container } from "./style";
+
 
 export default function Pesquisa(){
-
     return (
-        <></>         
+        <Container>
+            <HeaderGeral text="Pesquisa"/>
+            
+            <Footer/>
+        </Container>      
 
     );
 }
--- src/pages/TV/style.tsx
+++ src/pages/TV/style.tsx
@@ -1,5 +1,12 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: #F5F5F5;
+    overflow-x: hidden;
 `
