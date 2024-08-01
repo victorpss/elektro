addac5a5cf8dfd44ce772437a3182d9c0862ed9b
victorpss <victorpss@dcc.ufrj.br>
2024-08-01 02:34:37 -0300

    created TV page

--- src/pages/TV/index.tsx
+++ src/pages/TV/index.tsx
@@ -1,15 +1,38 @@
 import Footer from "../../components/Footer";
 import HeaderGeral from "../../components/HeaderGeral";
-import { Container } from "./style";
+import Produto from "../../components/Produto";
+import { Container, Filtro, PesquisaContainer, PesquisaInput, PesquisaInputField, PesquisaIcon, TVsDisplay } from "./style";
+
+import lupaIcon from "../../assets/icons/lupaCinza.png";
+import filtroIcon from "../../assets/icons/filtro.png";
+
+import tvLG from "../../assets/produtos/tvLG.png";
+import tvSamsung from "../../assets/produtos/tvSamsung.png";
+import tvTCL from "../../assets/produtos/tvTCL.png";
+import tvSemp from "../../assets/produtos/tvSemp.png";
+import tvAOC from "../../assets/produtos/tvAOC.png";
+import tvPhilips from "../../assets/produtos/tvPhilips.png";
 
 export default function TV(){
-
     return (
         <Container>
             <HeaderGeral text="Pesquisa"/>
-            
+            <PesquisaContainer>
+                <PesquisaInputField>
+                    <PesquisaIcon src={lupaIcon} />
+                    <PesquisaInput type="text" placeholder="Encontrar na Elektro"/>
+                </PesquisaInputField>
+                <Filtro src={filtroIcon}></Filtro>
+            </PesquisaContainer>
+            <TVsDisplay>
+                <Produto imagePath={tvLG} name="TV LG" price="R$2.000,00"/>
+                <Produto imagePath={tvSamsung} name="TV Samsung" price="R$1.400,00"/>
+                <Produto imagePath={tvTCL} name="TV TCL" price="R$1.000,00"/>
+                <Produto imagePath={tvSemp} name="TV Semp" price="R$1.200,00"/>
+                <Produto imagePath={tvAOC} name="TV AOC" price="R$1.300,00"/>
+                <Produto imagePath={tvPhilips} name="TV Philips" price="R$1.400,00"/>
+            </TVsDisplay>
             <Footer/>
-        </Container>          
-
+        </Container>
     );
 }
--- src/pages/TV/style.tsx
+++ src/pages/TV/style.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
+export const Container = styled.main`
     width: 100%;
     height: 100vh;
     display: flex;
@@ -9,4 +9,54 @@ export const Container = styled.div`
     align-items: center;
     background-color: #F5F5F5;
     overflow-x: hidden;
-`
+`;
+
+export const TVsDisplay = styled.section`
+    width: 80%;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    row-gap: 16px;
+    column-gap: 12px;
+`;
+
+export const PesquisaContainer = styled.div`
+    width: 80%;
+    display: flex;
+    justify-content: space-between;
+`;
+
+export const PesquisaInputField = styled.div`
+    width: 257px;
+    height: 40px;
+    background-color: #2B2B2B;
+    border-radius: 16px;
+    display: flex;
+    align-items: center;
+    position: relative;
+`;
+
+export const PesquisaIcon = styled.img`
+    position: absolute;
+    left: 10px;
+    width: 20px;
+    height: 20px;
+`;
+
+export const PesquisaInput = styled.input`
+    width: 100%;
+    height: 96%;
+    background-color: transparent;
+    color: #F5F5F5;
+    border: 0;
+    border-radius: 16px;
+    font-family: "Montserrat", sans-serif;
+    font-weight: 400;
+    font-size: 16px;
+    padding-left: 34px;
+`;
+
+export const Filtro = styled.img`
+    width: 57px;
+    height: 55px;
+    cursor: pointer;
+`;
